6960e2b45925cdd63c20bd4885005b1539425795
LauS4nchez <lautarosanchezbonaldo@gmail.com>
2025-10-07 01:17:47 -0300

    1.0.4

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // expone en 0.0.0.0
-    port: Number(process.env.PORT) || 5173, // usa PORT de Render
+    host: true,
+    port: Number(process.env.PORT) || 5173,
+    allowedHosts: ["futbolsk.onrender.com"], // <--- tu dominio de Render
   },
 });

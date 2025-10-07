9f13ebf6ea58af00408d584d17e5e7cfb51f375e
LauS4nchez <lautarosanchezbonaldo@gmail.com>
2025-10-07 01:01:01 -0300

    1.0.1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../lib/supaBaseClient";
 
 import "./Login.css";
 
